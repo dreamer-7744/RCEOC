6ec95cbf97d8ca52c8c0bbea5b612d4e35e7ec85
dreamer-7744 <jhyu@inu.ac.kr>
2025-05-22 13:26:43 +0900

    Update Executable_Experimental_Code.m

--- Executable_Experimental_Code.m
+++ Executable_Experimental_Code.m
@@ -1,6 +1,8 @@
+% This MATLAB code is for executable code for repeated 5 fold cross validation of RCE-OC algorithm. The users can change apply different datasets for the experiment by chaing the path of dataset %
+
 max_cross_iter = 5;
 K = 5;
 kmax = 20;
 
-data1 = dlmread('C:\Users\user\Desktop\MyWork\data\논문에 쓴 데이터\wine.csv');
+data1 = dlmread('C:\Users\user\Desktop\Codes and Datasets\Datasets\wine.csv');
 [final_AUC_result_all_cl] = Repeated_5foldCV_experiement_RNEOC(data1, max_cross_iter, K, kmax);
